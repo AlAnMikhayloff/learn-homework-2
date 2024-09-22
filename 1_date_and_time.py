58cee15f4329fc4a936d181a970687c803142cd7
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-22 21:00:13 +0300

    Update 1_date_and_time.py

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -13,15 +13,22 @@ def print_days():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    from datetime import datetime, timedelta
+    print(datetime.now().strftime('%d-%m-%Y'))
+    print((datetime.now() - timedelta(days=1)).strftime('%d-%m-%Y'))
+    print((datetime.now() - timedelta(days=30)).strftime('%d-%m-%Y'))
 
 
-def str_2_datetime(date_string):
+def str_2_datetime(date_string,):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    from datetime import datetime, timedelta
+    date_string = "01/01/20 12:10:03.234567"
+    date_dt = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+    print(date_dt)
+    
 
 if __name__ == "__main__":
     print_days()
