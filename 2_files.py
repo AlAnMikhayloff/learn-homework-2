d65d56cdbe7c0321f60119aa27083700a9bdcfe4
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-24 01:26:03 +0300

    aster

--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,28 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+        filename = 'referat2.txt'
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(content + '\n')
+            #f.write('\n')
+
+        len_str = str(len(content))
+        with open(filename, 'a', encoding='utf-8', newline='\n') as f:
+            f.write(len_str + '\n')
+            
+        number_of_word = str(len(content.split(' ')))
+        with open(filename, 'a', encoding='utf-8', newline='\n') as f:
+            f.write(number_of_word + '\n')
+           
+        change = content.replace('.', '!')
+        with open(filename, 'a', encoding='utf-8', newline='\n') as f:
+            f.write(change)
+    
+    
+    
+    
 
 if __name__ == "__main__":
     main()
