d48c5ea68d4bcf38ffb8249c84686a02c11eb7a7
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-24 22:37:04 +0300

    Домашнее задание .csv

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -15,7 +15,22 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    import csv
+    workers = [
+               {'name': 'Ben', 'age': '25', 'job': 'engineer', 'hobby': 'drawing'}, 
+               {'name': 'Steve', 'age': '36', 'job': 'programmer', 'hobby': 'diving'},
+               {'name': 'Rubby', 'age': '22', 'job': 'florist', 'hobby': 'classical music'},
+               {'name': 'Louis', 'age': '52', 'job': 'economist', 'hobby': 'rock music'},
+               {'name': 'Joe', 'age': '62', 'job': 'musician', 'hobby': 'washing dishes'}
+                ]
+    
+    with open('neighbours.csv', 'w', encoding='utf-8') as f:
+        fields = ['name', 'age', 'job', 'hobby']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for user in workers:
+            writer.writerow(user)
+
 
 if __name__ == "__main__":
     main()
